be6c88f3b3a043c24e31159fe37964ad8b0a5b7f
Joaovitorufu <joaovitorufu@gmail.com>
2021-06-28 10:58:11 -0300

    metodo com arrow function para utilizar o this dentro sem precisar de bind

--- src/App.js
+++ src/App.js
@@ -5,27 +5,35 @@ import { Component } from 'react';
 class App extends Component {
   constructor(props){
     super(props);
+    this.handlePClick = this.handlePClick.bind(this);
 
     this.state = {
-      name: 'João Vitor'
+      name: 'João Vitor',
+      counter: 0
     };
   }
 
-handlePClick (){ //metodo criado para dar um console log a partir do click = usado na linha 25
-  console.log('clicado');
+handlePClick (){
+  this.setState({name: 'Rafa'}); // para o this funcionar nesse caso teremos que fazer um bind com o this --linha 8
 }
 
+handleAClick= (event) => { 
+  event.preventDefault(); //preventdefault previne um evento que iria acontecer por default, ou seja, irá acontecer o que estiver dentro do metodo e não irá abrir a pagina para a documentação do react.
+  const {counter} = this.state;
+  this.setState({counter : counter +1}) //sempre que chamar esse metodo atualiza o contador com 1 a mais
+}
 render(){
-  const {name} = this.state;
+  const {name, counter} = this.state; //renderizando em tela os meus estados
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p onClick={this.handlePClick}> {/* onClick serve aqui para ativar o metodo quando ser clicado */}
-          {name}
+          {name} {counter}
         </p>
         <a
+          onClick={this.handleAClick}
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
