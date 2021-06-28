d540905cdcd73336cace826a0f8ff0a8aa0753bb
Joaovitorufu <joaovitorufu@gmail.com>
2021-06-28 16:33:55 -0300

    Redirecionando com o useHistory

--- src/templates/Abc/index.jsx
+++ src/templates/Abc/index.jsx
@@ -1,4 +1,15 @@
+import { useEffect } from "react";
+import { useHistory } from "react-router-dom";
+
 export const Abc = () => {
+    const history = useHistory();
+
+    useEffect(() => {
+        setTimeout(function () {
+            history.push('/');
+        }, 5000);
+    }, [history]);
+
     return(
         <div>
             <h1>ABC</h1>
