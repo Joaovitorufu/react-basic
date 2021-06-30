b274fc2cdbf32c6143a9214ff8b4a5289abde59e
Joaovitorufu <joaovitorufu@gmail.com>
2021-06-30 10:57:23 -0300

    css dos posts e adicionando fotos

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import './App.css';
+import { Component } from 'react';
+// import {PostCard} from './components/PostCard'
+// import { loadPosts } from './utils/load-posts';
+
+class App extends Component {
+
+    state = {
+      posts: []
+    };
+  
+  componentDidMount(){
+   this.loadPosts();
+  }
+
+  loadPosts = async () => {
+    const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts');
+    const photoResponse = fetch('https://jsonplaceholder.typicode.com/photos');
+
+    const [posts, photos] = await Promise.all([postsResponse, photoResponse]);
+
+    const postsJson = await posts.json();
+    const photosJson = await photos.json();
+
+    const postsAndPhotos = postsJson.map((post,index)=>{
+      return {...post, cover: photosJson[index].url}
+    });
+
+    this.setState({posts: postsAndPhotos});
+    
+  }
+
+render(){
+  const {posts} = this.state;
+  return (
+    <section className="container">
+    <div className="posts">
+      {posts.map(post =>(
+        <div className="post">
+          <img src={post.cover} alt={post.title} />
+          <div key={post.id} className="post-content">
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+    </section>
+  );
+}
+}
+
+export default App;
